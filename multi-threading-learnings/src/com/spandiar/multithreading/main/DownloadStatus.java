1d9d2013d6fa93e4b9677a1266b79c86ebe1d36f
SPANDIAR <spandiar0906@gmail.com>
2021-06-16 20:25:21 +0530

    Fixed the race condition using Reentrant Locks

--- multi-threading-learnings/src/com/spandiar/multithreading/main/DownloadStatus.java
+++ multi-threading-learnings/src/com/spandiar/multithreading/main/DownloadStatus.java
@@ -1,12 +1,17 @@
 package com.spandiar.multithreading.main;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class DownloadStatus {
 	
 	private long totalDownloadedBytes;
+	private Lock lock;
 
 	public DownloadStatus() {
 		super();
 		this.totalDownloadedBytes = 0;
+		this.lock = new ReentrantLock();
 	}
 
 	public long getTotalDownloadedBytes() {
@@ -14,7 +19,9 @@ public class DownloadStatus {
 	}
 
 	public  void incrementTotalDownloadedBytes() {
+		lock.lock();
 		this.totalDownloadedBytes++;
+		lock.unlock();
 	}
 	
 }
